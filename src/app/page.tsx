d2fc378da2c59840e6710227026e8e06cf5deebf
Felipe Siqueira <flipggs@gmail.com>
2023-08-08 08:55:25 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import rp  from 'request-promise';
 import $ from'cheerio';
 
 import puppeteer from 'puppeteer';
@@ -6,82 +5,40 @@ import puppeteer from 'puppeteer';
 export default async function Home() {
   const url = 'https://investidor10.com.br/acoes/petr4/';
 
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
 
-  // // Navigate the page to a URL
-  // await page.goto(url);
+  await page.goto(url);
+  const html = await page.content();
 
-  // const data = await page.evaluate(() => {
-  //   return {
-  //     teste: document.querySelector('#table-indicators .cell')?.innerHTML
-  //   }
-  // })
+  let object = {};
+  let key = '';
 
-  // console.log({ data });
+  $('#table-indicators span', html).each(function() {
+    const text = $(this).text(); 
+    const textSplited = text.split('\n');
 
-  // await browser.close();
+    textSplited.forEach((item) => {
+      if (item) {
+        const value = item.replaceAll('%', '').replaceAll(',', '.');
 
-  puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  }).then(function(html) {
-    let object = {};
-    let key = '';
-    $('#table-indicators span', html).each(function() {
-      const text = $(this).text(); 
-      const textSplited = text.split('\n');
+        if (isNaN(parseFloat(value))) {
+          key = value.trim();
+        } 
 
-      textSplited.forEach((item) => {
-        if (item) {
-          const value = item.replaceAll('%', '').replaceAll(',', '.');
-
-          if (isNaN(parseFloat(value))) {
-            key = value.trim();
-          } 
-
-          object = {
-            ...object,
-            [key]: isNaN(parseFloat(value)) ? '' : item
-          }
+        object = {
+          ...object,
+          [key]: isNaN(parseFloat(value)) ? '' : item
         }
-      });
+      }
     });
-
-    console.log({ object });
-  })
-  .catch(function(err) {
-    //handle error
-  });
-
-
-  // rp(url)
-  // .then(function(html){
-  //   //success!
-  //   const body = $.load(html);
-  //   const cell = body('#table-indicators .cell');
-
-  //   cell.each(() => {
-  //     console.log('--------------------------------------------------------------------------------');
-  //     console.log('item', cell.text())
-  //   })
-
-  // })
-  // .catch(function(err){
-  //   console.log({ err });
-  // });
-  
-    
+  });    
 
   return (
    <div>
-    <h1>teste</h1>
+    <h1>Crawler do site</h1>
+    <a href={url} target='_blank'>{url}</a>
+    <pre>{JSON.stringify(object, null, 2)}</pre>
    </div>
   )
 }
